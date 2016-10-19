056e9b888e111e7c8bd69bc56ebf50897391a56e
Cameron Ritchie <critchie0258@gmail.com>
2016-10-19 11:59:45 -0700

    completed

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,21 @@
 // If a number is divisible by 3, instead of the number, output "Fizz".
 // If a number is divisible by 5, output "Buzz".
 // If a number is divisible by both 3 and 5, output "FizzBuzz".
+var content = document.getElementById("content");
+
+for(i = 1; i<=100; i++ ){
+  //console.log(i);
+  if(i % 3 === 0 && i % 5 === 0){
+    content.innerHTML = content.innerHTML + " FizzBuzz " + " ";
+  }
+  else if(i % 5 === 0){
+    content.innerHTML = content.innerHTML + " Buzz ";
+  }
+  else if(i % 3 === 0)
+  {
+    content.innerHTML = content.innerHTML + " Fizz ";
+  }
+  else{
+    content.innerHTML = content.innerHTML + " " + i + " ";
+  }
+}
